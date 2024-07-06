d3242f8bb96d0e4f6790ae0f3bbe85a2e3ed6df6
ANURAGTIGGA <a.aront003@gmail.com>
2024-07-06 22:38:14 +0530

    added getMessage and user route and controller

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -26,12 +26,35 @@ export const sendMessage = async (req, res) => {
         if(newMessage) {
             conversation.messages.push(newMessage._id);
         }
-        await conversation.save();
-        await newMessage.save();
-        
+        // await conversation.save();
+        // await newMessage.save();
+
+        // they will run in parallel
+        await Promise.all([conversation.save(), newMessage.save()]);
+
         res.status(201).json(newMessage);
     } catch(error) {
         console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try {
+        const {id: userToChatId} = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] }
+        }).populate("messages"); // not references but actual messages obj array
+
+        if(!conversation) {
+            return res.status(200).json([]);
+        }
+
+        res.status(200).json(conversation.messages);
+    } catch(error) {
+        console.log("Error in getMessage controller: ", error.message);
         res.status(500).json({error: "Internal Server Error"});
     }
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import userRoutes from './routes/user.route.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(cookieParser()); // to parse incoming cokkies from req.cookies
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 // app.get('/', (req, res)=>{
 //     res.send("Hello world");
